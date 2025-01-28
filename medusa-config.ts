e93588918dd34fe6fdac5cb8929830223fbaf35b
treedon <trentmw@me.com>
2025-01-28 15:09:10 -0700

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -11,6 +11,7 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+      redis_url: REDIS_URL
     }
   }
 })
